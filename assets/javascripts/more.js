45613d7d6cc741132eba41d512d6358b798eaf9a
bertrand jung <bertrand.jung@epitech.eu>
2015-11-26 16:05:04 +0100

    mdl refactoring - replace both bootstrap classes with mdl

--- assets/javascripts/more.js
+++ assets/javascripts/more.js
@@ -3,9 +3,9 @@ define(['jquery'], function ($) {
   $(".more").on("click", function(e) {
     e.preventDefault()
     console.log($(this))
-    var parent = $(this).closest(".row")
+    var parent = $(this).closest(".mdl-grid")
     console.log(parent)
-    var sibling = parent.next(".row")
+    var sibling = parent.next(".mdl-grid")
     sibling.toggleClass("hidden")
   });
 
